892e98d25346e5e3442fed35dc39d8fd0fd81501
simonolson1 <41298245+simonolson1@users.noreply.github.com>
2018-09-06 17:25:05 +0200

    clear och fina färger i sudokun

--- SudokoProjekt/src/Sudoko/SudokoSolverController.java
+++ SudokoProjekt/src/Sudoko/SudokoSolverController.java
@@ -1,55 +1,79 @@
 package Sudoko;
+
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class SudokoSolverController extends Application {
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		int xaxis = 450;
 		int yaxis = 450;
-		
+
 		HBox hbox = new HBox(8);
 		Button button1 = new Button("Solve");
 		Button button2 = new Button("Clear");
 		hbox.getChildren().addAll(button1, button2);
-		
-		
+
 		TilePane tile = new TilePane();
-				
-		for (int i = 0; i < 81; i++){
-			TextField textBox = new TextField();
-			textBox.setPrefWidth(xaxis/9);
-			textBox.setPrefHeight(yaxis/9);
-			
+
+		boolean switchColor = true;
+		Paint value0 = Paint.valueOf("ff8668"); // bakgrundsfärg
+
+		for (int i = 1; i <= 81; i++) {
+			OneNumberTextField textBox = new OneNumberTextField();
+			textBox.setPrefWidth(xaxis / 9);
+			textBox.setPrefHeight(yaxis / 9);
+			if (switchColor) {
+				textBox.setStyle("-fx-control-inner-background: #" + value0.toString().substring(2));
+			}
+
+			if (i % 3 == 0) {
+				switchColor = !switchColor;
+			}
+			if (i % 9 == 0) { // inte min vackraste kod
+				if (i == 27 || i == 36 || i == 45) {
+					switchColor = false;
+				} else {
+					switchColor = true;
+				}
+			}
+
 			tile.getChildren().add(textBox);
 		}
-		
-		
+
 		BorderPane root = new BorderPane();
 		root.setCenter(tile);
 		root.setBottom(hbox);
-		
+
 		button1.setOnAction(event -> {
 			
 		});
 		button2.setOnAction(event -> {
-			
+			clear(tile);
 		});
-		
+
 		Scene scene = new Scene(root, xaxis, yaxis + 25);
 		primaryStage.setTitle("SudokoSolver");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	private void clear(TilePane tile){
+		for (Node node : tile.getChildren()) {
+		    if (node instanceof OneNumberTextField) {
+		        ((OneNumberTextField)node).setText("");
+		    }
+		}
 	}
 
 	public static void main(String[] args) {
